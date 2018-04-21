cf9090da461abdb6b2340d9f8fa5b9ea26ad7cdd
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-04-20 23:48:08 -0300

    Publish v0.1.0

--- dist/reconstruct-descriptors.es.js
+++ dist/reconstruct-descriptors.es.js
@@ -1,5 +1,5 @@
 /*!
- * reconstruct-descriptors v0.0.0
+ * reconstruct-descriptors v0.1.0
  * (c) 2018-present Vitor Luiz Cavalcanti <vitorluizc@outlook.com> (https://vitorluizc.github.io)
  * Released under the MIT License.
  */
