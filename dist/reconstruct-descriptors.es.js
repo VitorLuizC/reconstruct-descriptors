bb2b19560417932aa13f28dc5e96768718adf3dd
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-04-20 22:49:57 -0300

    Create build strategy

---
+++ dist/reconstruct-descriptors.es.js
@@ -0,0 +1,8 @@
+/*!
+ * reconstruct-descriptors v0.0.0
+ * (c) 2018-present Vitor Luiz Cavalcanti <vitorluizc@outlook.com> (https://vitorluizc.github.io)
+ * Released under the MIT License.
+ */
+var reconstructDescriptors = (function () { return null; });
+
+export default reconstructDescriptors;
---
+++ dist/reconstruct-descriptors.js
@@ -0,0 +1,16 @@
+/*!
+ * reconstruct-descriptors v0.0.0
+ * (c) 2018-present Vitor Luiz Cavalcanti <vitorluizc@outlook.com> (https://vitorluizc.github.io)
+ * Released under the MIT License.
+ */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.reconstructDescriptors = factory());
+}(this, (function () { 'use strict';
+
+	var reconstructDescriptors = (function () { return null; });
+
+	return reconstructDescriptors;
+
+})));
